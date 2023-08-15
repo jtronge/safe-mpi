d33450626f948b344e98df6b9cacfd79a9dd1e50
Jake Tronge <jtronge3@gmail.com>
2023-08-15 15:27:16 -0600

    Add new latency results

--- scripts/latency_bincode.sh
+++ scripts/latency_bincode.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #SBATCH -n 2
 #SBATCH -N 1
+export UCX_TLS=rc
 
 source $SAFE_MPI_ENV_FILE
 ./target/release/latency_serde -k bincode -c $SAFE_MPI_CONFIG -s -p 8888 127.0.0.1 &
--- scripts/latency_rsmpi.sh
+++ scripts/latency_rsmpi.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #SBATCH -n 2
 #SBATCH -N 1
+export UCX_TLS=rc
+export OMPI_MCA_pml=ucx
 
 source $SAFE_MPI_ENV_FILE
 mpirun -np 2 ./target/release/latency_rsmpi -c $SAFE_MPI_CONFIG
