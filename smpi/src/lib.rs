74b670e4c41ba75ee3e196e1ac21b6e937420592
Jake Tronge <jtronge3@gmail.com>
2024-02-27 18:37:39 -0700

    Add empty smpi-tests crate

--- smpi/src/lib.rs
+++ smpi/src/lib.rs
@@ -4,19 +4,21 @@ pub enum Error {}
 
 type Result<T> = std::result::Result<T, Error>;
 
-struct ProcessGroup;
+struct Communicator;
 
-impl ProcessGroup {
+impl Communicator {
     /// Return number of members in the process group.
     pub fn size(&self) -> u64 { 1 }
 
     /// Return the ID of this process.
     pub fn id(&self) -> u64 { 0 }
 
+    /// Send a message to another process.
     pub fn send<T: Message>(&self, message: &T, target: u64) -> Result<()> {
         Ok(())
     }
 
+    /// Receive a message from another process.
     pub fn recv<T: Message>(&self, message: &mut T, source: u64) -> Result<()> {
         Ok(())
     }
