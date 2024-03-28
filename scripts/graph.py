efebd47cd27032f35781ddeda26bf4a5ac87b0ff
Jake Tronge <jtronge3@gmail.com>
2024-03-28 11:11:22 -0600

    Add updated graphing code

--- scripts/graph.py
+++ scripts/graph.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 import json
 
 
@@ -13,40 +14,72 @@ fmts = {
 }
 
 
-def graph_latency(config, tests):
+def show_change(sizes, results):
+    """Show percentage change in results from the rsmpi baseline."""
+    if 'rsmpi' in results:
+        print('======================================================')
+        for test_name in results:
+            if test_name == 'rsmpi':
+                continue
+            change = 100 * (results[test_name] - results['rsmpi']) / results['rsmpi']
+            print(test_name)
+            for size, percent in zip(sizes, change):
+                print(f'==> {size}: {percent:.2f}%')
+
+
+def compute_error(results):
+    """Compute the average and error."""
+    result = np.average(results, 0)
+    max_result = np.amax(results, axis=0)
+    min_result = np.amin(results, axis=0)
+    error = [result - min_result, max_result - result]
+    return result, error
+
+
+def graph_latency(title, tests):
     """Graph a latency result."""
     fig, ax = plt.subplots()
-    ax.set_title(f'latency: {config}')
+    ax.set_title(f'latency: {title}')
     ax.set_xlabel('size (bytes)')
     ax.set_ylabel('latency (μs)')
     ax.grid(visible=True)
+    lat_results = {}
     for test_name, results in tests.items():
-        sizes = sorted(int(size) for size in results)
+        sizes = sorted(int(size) for size in results['size'])
         sizes = [str(size) for size in sizes]
-        lats = [results[size] for size in sizes]
-        ax.plot(sizes, lats, fmts[test_name], label=test_name)
+        print(results['data'])
+        all_lats = np.array(results['data'])
+        lats, error = compute_error(all_lats)
+        lat_results[test_name] = lats
+        ax.errorbar(sizes, lats, yerr=error, fmt=fmts[test_name], label=test_name)
+    show_change(sizes, lat_results)
     ax.legend()
     plt.show()
 
 
-def graph_bw(config, tests):
+def graph_bw(title, tests):
     """Graph a bandwidth result."""
     fig, ax = plt.subplots()
-    ax.set_title(f'bandwidth: {config}')
+    ax.set_title(f'bandwidth: {title}')
     ax.set_xlabel('size (bytes)')
     ax.set_ylabel('bandwidth (MB/s)')
     ax.grid(visible=True)
+    bw_results = {}
     for test_name, results in tests.items():
-        sizes = sorted(int(size) for size in results)
+        sizes = sorted(int(size) for size in results['size'])
         sizes = [str(size) for size in sizes]
-        bw = [results[size] for size in sizes]
-        ax.plot(sizes, bw, fmts[test_name], label=test_name)
+        all_bw = np.array(results['data'])
+        bw, error = compute_error(all_bw)
+        bw_results[test_name] = bw
+        ax.errorbar(sizes, bw, yerr=error, fmt=fmts[test_name], label=test_name)
+    show_change(sizes, bw_results)
     ax.legend()
     plt.show()
 
 
 parser = argparse.ArgumentParser(description='graph benchmark results')
 parser.add_argument('-o', '--output', help='JSON benchmark output', required=True)
+parser.add_argument('-t', '--title', help='graph title (uses default if left unspecified)')
 args = parser.parse_args()
 
 graphers = {
@@ -62,4 +95,4 @@ plt.rcParams.update({
 })
 for benchmark, configs in results.items():
     for config, tests in configs.items():
-        graphers[benchmark](config, tests)
+        graphers[benchmark](config if args.title is None else args.title, tests)
